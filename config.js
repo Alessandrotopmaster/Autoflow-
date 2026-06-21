f51f6ddbedc22cc3a7596196d4ce15b108713e2f
Alessandro Eustaquio <pixsandro77@gmail.com>
2026-06-20 23:40:45 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    planos: { basico: 29.90, avancado: 59.90, premium: 89.90 },
+    niveis: { iniciante: 0.08, intermediario: 0.12, avancado: 0.15 },
+    metas: { vendas: 500, pedidos: 10 }
+};
